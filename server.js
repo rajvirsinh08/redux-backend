7a93d0d132d973aaa0b56f1c2e2d4cc4d5053253
Rajvir Dabhi <rajvirsinhdabhi1@gmail.com>
2024-07-15 16:59:08 +0530

    update server

--- server.js
+++ server.js
@@ -1,39 +1,3 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
-// const cors = require("cors");
-// const userRoute = require("./Routes/userRoutes");
-
-// dotenv.config();
-// const app = express();
-
-// app.use(express.json());
-// app.use(cors({
-//     origin: "*",
-//     methods: ["POST", "GET", "DELETE", "PUT", "PATCH"],
-//     credentials: true,
-// }));
-// app.use('/uploads', express.static('uploads'));
-// app.use(userRoute);
-
-// mongoose.connect(process.env.URI)
-//     .then(() => {
-//         console.log("Connected to MongoDB");
-
-//         app.listen(process.env.PORT || 8000, () => {
-//             console.log(`Server running on port ${process.env.PORT || 8000}`);
-//         });
-//     })
-//     .catch(error => {
-//         console.error("Error connecting to MongoDB:", error);
-//     });
-
-// server.get('/', (req, res) => {
-//     res.send('Hello from Express on Vercel!');
-// });
-
-
-// module.exports = app; // Export the Express app instance
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
