e4d6215e8a2e6d143bd7a0a7cefcf46c89e5e51f
Rajvir Dabhi <rajvirsinhdabhi1@gmail.com>
2024-07-19 12:32:03 +0530

    adjust server

--- server.js
+++ server.js
@@ -1,3 +1,34 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+// const cors = require("cors");
+// const userRoute = require("./Routes/userRoutes");
+
+// dotenv.config();
+// const app = express(); 
+
+// app.use(express.json());
+// app.use(cors({
+//     origin: "*",
+//     methods: ["POST", "GET", "DELETE", "PUT", "PATCH"],
+//     credentials: true,
+// }));
+// app.use('/uploads', express.static('uploads'));
+// app.use(userRoute);
+
+// mongoose.connect(process.env.URI)
+//     .then(() => {
+//         console.log("Connected to MongoDB");
+
+//         app.listen(process.env.PORT || 8000, () => {
+//             console.log(`Server running on port ${process.env.PORT || 8000}`);
+//         });
+//     })
+//     .catch(error => {
+//         console.error("Error connecting to MongoDB:", error);
+//     });
+
+// module.exports = app; // Export the Express app instanceconst express = require("express");
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
@@ -14,9 +45,9 @@ app.use(cors({
     credentials: true,
 }));
 app.use('/uploads', express.static('uploads'));
-app.use(userRoute);
+app.use('/api/users', userRoute);
 
-mongoose.connect(process.env.URI)
+mongoose.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Connected to MongoDB");
 
